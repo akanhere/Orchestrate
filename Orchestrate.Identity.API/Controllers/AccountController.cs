1e2dd1d7e8efb76b97ad078324e632ce9f3e631d
akanhere <ashwin.kanhere@gmail.com>
2020-08-01 16:43:35 +0530

    registration functionality

--- Orchestrate.Identity.API/Controllers/AccountController.cs
+++ Orchestrate.Identity.API/Controllers/AccountController.cs
@@ -2,12 +2,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Orchestrate.Identity.API.Models.AccountViewModels;
 
 namespace Orchestrate.Identity.API.Controllers
 {
     public class AccountController : Controller
     {
+
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public AccountController(UserManager<IdentityUser> userManager)
+        {
+            _userManager = userManager;
+        }
         public IActionResult Index()
         {
             return View();
@@ -16,6 +26,58 @@ namespace Orchestrate.Identity.API.Controllers
         public async Task<IActionResult> Login()
         {
             return View();
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register(string returnUrl = null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        
+        public async Task<IActionResult> Register(RegisterViewModel model, string returnUrl=null)
+        {
+            ViewData["ReturnUrl"] = returnUrl;
+            if (ModelState.IsValid)
+            {
+                var user = new IdentityUser
+                {
+                    UserName=model.Email,
+                    Email=model.Email,
+                    PhoneNumber = model.User.PhoneNumber
+                };
+                var result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Errors.Count() > 0)
+                {
+                    AddErrors(result);
+                    // If we got this far, something failed, redisplay form
+                    return View(model);
+                }
+            }
+            if (returnUrl != null)
+            {
+                if (HttpContext.User.Identity.IsAuthenticated)
+                    return Redirect(returnUrl);
+                else
+                    if (ModelState.IsValid)
+                    return RedirectToAction("login", "account", new { returnUrl = returnUrl });
+                else
+                    return View(model);
+            }
+
+            return RedirectToAction("index", "home");
+
+        }
+        private void AddErrors(IdentityResult result)
+        {
+            foreach (var error in result.Errors)
+            {
+                ModelState.AddModelError(string.Empty, error.Description);
+            }
         }
     }
 }
